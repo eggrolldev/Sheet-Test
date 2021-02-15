b57a3e261ef86b9f2bc4799567ee12788f651f22
eggrolldev <78978156+eggrolldev@users.noreply.github.com>
2021-02-15 01:36:53 -0500

    Update HalfASheet.swift

--- Sources/HalfASheet/HalfASheet.swift
+++ Sources/HalfASheet/HalfASheet.swift
@@ -7,6 +7,33 @@
 import SwiftUI
 import Combine
 
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+
+        return nil
+    }
+}
 
 public struct HalfASheet<Content: View>: View {
     
@@ -16,7 +43,7 @@ public struct HalfASheet<Content: View>: View {
     
     internal var height: HalfASheetHeight = .proportional(0.25) // about the same as a ColorPicker
     internal var contentInsets = EdgeInsets(top: 7, leading: 16, bottom: 12, trailing: 16)
-    internal var backgroundColor: UIColor = .gray
+    internal var backgroundColor: UIColor = UIColor(hex: "#ffe700ff")
     internal var closeButtonColor: UIColor = .gray
     internal var allowsDraggingToDismiss = true
     
